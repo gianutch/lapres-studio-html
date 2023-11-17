0c5c96d3951dfe7f7e1fe4f506d36c0a122c607f
gianutch <design@lebowyer.com>
2023-11-17 16:58:06 +0000

    2023.11.17-1

---
+++ js/launcher.js
@@ -0,0 +1,5 @@
+/* SCROLLSMOOTH ============================================================ */
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 300 /* speed is ms per 1000px */,
+	speedAsDuration: true /* speed is now duration of animation */,
+});
